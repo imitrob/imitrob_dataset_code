a5425328965312ff22af35ad77e142f00d0f19bd
Gabriela Sejnova <38910946+gabinsane@users.noreply.github.com>
2021-06-28 16:14:10 +0200

    download miniimagenet

--- download_miniimagenet.sh
+++ download_miniimagenet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ./miniimagenet
-wget -r -nH --cut-dirs=100  https://data.ciirc.cvut.cz/public/groups/incognite/Imitrob/mini-imagenet.zip -P ./miniimagenet
-unzip ./miniimagenet/mini-imagenet.zip
-rm /miniimagenet/mini-imagenet.zip
+wget -r -nH --cut-dirs=100  https://data.ciirc.cvut.cz/public/groups/incognite/Imitrob/mini-imagenet.zip
+unzip mini-imagenet.zip
+mv images miniimagenet
+rm mini-imagenet.zip
