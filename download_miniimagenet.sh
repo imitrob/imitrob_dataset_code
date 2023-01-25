63f55989d5f5e512e23d981f081f34f37a23868c
Radoslav Skoviera <radoslav.skoviera@cvut.cz>
2023-01-25 15:30:19 +0100

    Certificate expired, need to omit check

--- download_miniimagenet.sh
+++ download_miniimagenet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -r -nH --cut-dirs=100  https://data.ciirc.cvut.cz/public/groups/incognite/Imitrob/mini-imagenet.zip
+wget -r -nH --cut-dirs=100 --no-check-certificate https://data.ciirc.cvut.cz/public/groups/incognite/Imitrob/mini-imagenet.zip
 unzip mini-imagenet.zip
 mv images miniimagenet
 rm mini-imagenet.zip
